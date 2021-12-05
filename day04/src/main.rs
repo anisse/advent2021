d53710e49b3dbc0e867d1ec43ceba8c37a402603
Anisse Astier <anisse@astier.eu>
2021-12-05 16:30:50 +0100

    day4 part 1

---
+++ day04/src/main.rs
@@ -0,0 +1,93 @@
+type Board = Vec<Vec<u8>>;
+type BoardRef<'a> = &'a [Vec<u8>];
+
+fn main() {
+    let (boards, draws) = parse(include_str!("../input.txt"));
+    //part 1
+    let win_score = winning_score(&boards, &draws);
+    println!("Winning board score: {}", win_score);
+}
+fn parse(input: &str) -> (Vec<Board>, Vec<u8>) {
+    let mut lines = input.lines();
+
+    let draws = lines
+        .next()
+        .expect("No draw line")
+        .split(',')
+        .map(|x| x.parse::<u8>().expect("not int"))
+        .collect();
+
+    let mut boards = Vec::new();
+
+    while let Some(space) = lines.next() {
+        assert_eq!(space, ""); // skip space lines
+        let board = lines
+            .by_ref()
+            .take(5)
+            .map(|row| {
+                row.split_ascii_whitespace()
+                    .map(|x| x.parse::<u8>().expect("not int"))
+                    .collect()
+            })
+            .collect();
+        boards.push(board);
+    }
+
+    (boards, draws)
+}
+
+fn winning_score(boards: &[Board], draws: &[u8]) -> usize {
+    let mut drawn = vec![vec![vec![false; 5]; 5]; boards.len()];
+    for draw in draws.iter() {
+        for (i, board) in boards.iter().enumerate() {
+            if let Some((row, col)) = board_has(board, *draw) {
+                drawn[i][row][col] = true;
+                if is_winning(&drawn[i], row, col) {
+                    return score_sum(board, &drawn[i]) * board[row][col] as usize;
+                }
+            }
+        }
+    }
+    0
+}
+
+fn board_has(board: BoardRef, num: u8) -> Option<(usize, usize)> {
+    for (row, l) in board.iter().enumerate() {
+        for (col, v) in l.iter().enumerate() {
+            if *v == num {
+                return Some((row, col));
+            }
+        }
+    }
+    None
+}
+
+fn is_winning(drawn: &[Vec<bool>], row: usize, col: usize) -> bool {
+    // check row
+    drawn[row].iter().all(|i| *i) ||
+        //check col
+        drawn.iter().all(|i| i[col])
+}
+fn score_sum(board: BoardRef, drawn: &[Vec<bool>]) -> usize {
+    board
+        .iter()
+        .zip(drawn.iter())
+        .map(|(row, d_row)| {
+            row.iter()
+                .zip(d_row.iter())
+                .filter(|(_, d)| !**d)
+                .map(|(x, _)| *x as usize)
+                .sum::<usize>()
+        })
+        .sum()
+}
+
+#[test]
+fn test() {
+    let (boards, draws) = parse(include_str!("../sample.txt"));
+    dbg!(&draws);
+    dbg!(&boards);
+    //part 1
+    let win_score = winning_score(&boards, &draws);
+    assert_eq!(win_score, 4512);
+}
